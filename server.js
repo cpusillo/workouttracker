574a307676b094b7d69a0d170e676eb8d9a6f57e
Corrine Pusillo <cpusillo@gmail.com>
2021-01-13 13:37:18 -0500

    initial commit of files

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "workouttracker";
+const collections = ["workouts"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.send(index.html);
+});
+
+
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+  });
+  
