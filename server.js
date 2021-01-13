099dfffb8c1c2ae1631d9a61bbbf31792edffabd
cpusillo <cpusillo@gmail.com>
2021-01-13 14:21:46 -0500

    finished all static routing

--- server.js
+++ server.js
@@ -22,13 +22,38 @@ db.on("error", error => {
   console.log("Database Error:", error);
 });
 
+// Serve our root route
 app.get("/", (req, res) => {
   res.send(index.html);
 });
 
+// Server our exercise route
 app.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/stats.html"));
+});
+
+// Show all workouts in the database
+app.get("/api/workouts", (req, res) => {
+  db.workouts.find({}, (err,data) => {
+    if (err) console.log(err);
+    res.json(data);
+  });
 })
+
+// Insert an entry into the database
+app.post("/api/workouts", ({ body }, res) => {
+  // Save the request body as an object called book
+   const exercise = body; // This includes the title and the author and the created
+  db.workouts.insert(exercise, (err, data) => {
+    if (err) console.log(err);
+    res.json(exercise)
+});
+});
+
 
 app.listen(3000, () => {
     console.log("App running on port 3000!");
