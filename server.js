ecba282508ce938e890ebeb821cca7415e90fb2e
cpusillo <cpusillo@gmail.com>
2021-01-16 10:26:23 -0500

    Deploying to Heroku

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutsdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Bring in our routes
 require("./routes/api-routes")(app);
