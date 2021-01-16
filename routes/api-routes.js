e8efac3978d44fe8eaaa230bc4d12689cb268b37
cpusillo <cpusillo@gmail.com>
2021-01-16 09:46:15 -0500

    Added api/workouts/range and totalduration logic

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,24 @@
 const db = require("../models");
 
 module.exports = function (app) {
-    // Show all workouts
+
+// Route to get all workouts
 app.get("/api/workouts", (req, res) => {
-    db.Exercise.find({}).then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        console.log(err)
+  db.Exercise.find({}).then(data => {
+    // Loop through the returned data and continually add to the totalduration
+    data.forEach(exercise => {
+        var total = 0;
+        exercise.exercises.forEach(e => {
+            total += e.duration;
+        });
+        exercise.totalDuration = total;
     });
-  })
+    // Actually returns the data from our db
+    res.json(data);
+      }).catch(err => {
+        console.log(err);
+      });
+  });
   
   // Insert a workout
   app.post("/api/workouts", ({ body },res) => {
@@ -30,4 +39,15 @@ app.get("/api/workouts", (req, res) => {
       console.log(err)
     });
   });
+
+  // Route to get the workouts range 
+  app.get("/api/workouts/range", (req, res) => {
+    db.Exercise.find({}).then(data => {
+        res.json(data);
+    }).catch(err => {
+        console.log(err);
+    });
+
+});
+
 }
